58e791c166518f2b304f10f5bc10fcb45eca0f1b
Ynara Lira Ventura <79769248+ynaralira@users.noreply.github.com>
2024-07-31 08:21:13 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,11 +1,8 @@
 <?php
 
-$localhost = "localhost";
-$user = "root";
-$pass = "";
-$db_name = "controle_financeiro";
-
-
+$localhost = "sql104.infinityfree.com";
+$user = "if0_37013198";
+$pass = "EuVE6ADFuwaKsYk";
+$db_name = "if0_37013198_controle_financeiro";
 $conexao = mysqli_connect($localhost, $user, $pass, $db_name);
-
 ?>
