d31294d8dd96ea98fb03422886f6923532b2e234
Ynara lira <ynaraliraventura@gmail.com>
2024-11-27 19:02:23 -0300

    Muda conexao com bf

--- back-end/conexao.php
+++ back-end/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$user = 'root';
-$pass = 'root';
+$user = 'ynara';
+$pass = 'Ynara@2019';
 $db_name = "controle_financeiro";
-$localhost = 'localhost';
+$localhost = 'localhost:3306';
 
 $conexao = mysqli_connect($localhost, $user, $pass, $db_name);
 ?>
